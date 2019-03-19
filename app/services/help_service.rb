e268a52454bbe54d3720fbd870b825d12f345847
Thiago-Cardoso <trovador182@gmail.com>
2019-03-18 23:51:22 -0300

    adjustment in file help_service

--- app/services/help_service.rb
+++ app/services/help_service.rb
@@ -2,9 +2,14 @@ class HelpService
     def self.call
       response  = "*Comandos* \n\n"
       response += "help\n"
-      response += "`Mostra a lista de palavras`\n\n"
+      response += "`Lista de comandos que eu conheço`\n\n"
+      response += "Tradução\n"
       response += "Traduza a palavra de x de inglês para português\n"
       response += "`Como é a palavra x em inglês`\n\n"
+      response += "remova ID\n"
+      response += "`Remove uma palavra baseada no ID dela`\n\n"
+      response += "listagem\n"
+      response += "`Mostra a lista de palavras`\n\n"
       response
     end
   end
