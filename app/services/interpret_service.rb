61fc783f6d4f31695439ad2dd8b35a7bf6606c50
Thiago-Cardoso <trovador182@gmail.com>
2019-03-18 23:54:05 -0300

    adjustment in file interpret_service

--- app/services/interpret_service.rb
+++ app/services/interpret_service.rb
@@ -2,8 +2,12 @@ class InterpretService
 
     def self.call action, params
       case action
+      when "list", "search"
+        WordModule::ListService.new(params, action).call()
       when "create"
         WordModule::CreateService.new(params).call()
+      when "remove"
+        WordModule::RemoveService.new(params).call()
       when "help"
         HelpService.call()
       else
